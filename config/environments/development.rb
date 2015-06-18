6386ebe2a8ac816dc18ad46381467a13335b9a31
thiennv57 <thienbg93@gmail.com>
2015-06-18 16:04:09 +0700

    Final commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'thienblog.herokuapp.com/'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
   # Settings specified here will take precedence over those in config/application.rb.
 
